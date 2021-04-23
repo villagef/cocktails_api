c59c42ef2b22b6c2fe708e66e520b86e4e38fd1b
villagef <filwyd123@gmail.com>
2021-04-23 20:17:31 +0200

    id fix

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+server.use(jsonServer.bodyParser)
+
+server.post('/post/user', (req, res) => {
+    if (req.method === 'POST') {
+      let userId = req.body['userId'];
+      if (userId != null && userId >= 0) {
+        let result = db.users.find(user => {
+          return user.userId == userId;
+        })
+  
+        if (result) {
+          let {id, ...user} = result;
+          res.status(200).jsonp(user);
+        } else {
+          res.status(400).jsonp({
+            error: "Bad userId"
+          });
+        }
+      } else {
+        res.status(400).jsonp({
+          error: "No valid userId"
+        });
+      }
+    }
+  });
